43377a44f8ef984d008eb4c352b0bad2a4266b52
Jacob Beers <jacobtbeers@gmail.com>
2024-08-28 17:52:00 -0500

    Add open-saml

---
+++ java/cbsso-opensaml/app/src/main/java/cbsso/opensaml/AuthNRequestGenerator.java
@@ -0,0 +1,89 @@
+package cbsso.opensaml;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.opensaml.core.config.ConfigurationService;
+import org.opensaml.core.config.InitializationException;
+import org.opensaml.core.config.InitializationService;
+import org.opensaml.core.xml.config.XMLObjectProviderRegistry;
+import org.opensaml.saml.saml2.core.AuthnRequest;
+import org.opensaml.saml.saml2.core.Issuer;
+import org.opensaml.saml.saml2.core.NameIDPolicy;
+import org.opensaml.saml.saml2.core.NameIDType;
+
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+import net.shibboleth.utilities.java.support.xml.BasicParserPool;
+import net.shibboleth.utilities.java.support.xml.ParserPool;
+
+public class AuthNRequestGenerator {
+    public static String generateAuthNRequest(String issuerId, String requestId)
+            throws InitializationException, ComponentInitializationException {
+        initOpenSAML();
+        AuthnRequest authnRequest = buildAuthnRequest(issuerId, requestId);
+        return OpenSAMLUtils.stringifySAMLObject(authnRequest);
+    }
+
+    private static void initOpenSAML() throws InitializationException, ComponentInitializationException {
+        XMLObjectProviderRegistry registry = new XMLObjectProviderRegistry();
+        ConfigurationService.register(XMLObjectProviderRegistry.class, registry);
+
+        registry.setParserPool(getParserPool());
+        InitializationService.initialize();
+    }
+
+    private static ParserPool getParserPool() throws ComponentInitializationException {
+        BasicParserPool parserPool = new BasicParserPool();
+        parserPool.setMaxPoolSize(100);
+        parserPool.setCoalescing(true);
+        parserPool.setIgnoreComments(true);
+        parserPool.setIgnoreElementContentWhitespace(true);
+        parserPool.setNamespaceAware(true);
+        parserPool.setExpandEntityReferences(false);
+        parserPool.setXincludeAware(false);
+
+        final Map<String, Boolean> features = new HashMap<String, Boolean>();
+        features.put("http://xml.org/sax/features/external-general-entities", Boolean.FALSE);
+        features.put("http://xml.org/sax/features/external-parameter-entities", Boolean.FALSE);
+        features.put("http://apache.org/xml/features/disallow-doctype-decl", Boolean.TRUE);
+        features.put("http://apache.org/xml/features/validation/schema/normalized-value", Boolean.FALSE);
+        features.put("http://javax.xml.XMLConstants/feature/secure-processing", Boolean.TRUE);
+
+        parserPool.setBuilderFeatures(features);
+
+        parserPool.setBuilderAttributes(new HashMap<String, Object>());
+
+        parserPool.initialize();
+
+        return parserPool;
+    }
+
+    private static AuthnRequest buildAuthnRequest(String issuerId, String requestId) {
+        AuthnRequest authnRequest = OpenSAMLUtils.buildSAMLObject(AuthnRequest.class);
+        authnRequest.setIssueInstant(Instant.now());
+        // authnRequest.setDestination(IPD_SSO_DESTINATION);
+        // authnRequest.setProtocolBinding(SAMLConstants.SAML2_ARTIFACT_BINDING_URI);
+        // authnRequest.setAssertionConsumerServiceURL(SP_ASSERTION_CONSUMER_SERVICE_URL);
+        authnRequest.setID(requestId);
+        authnRequest.setIssuer(buildIssuer(issuerId));
+        authnRequest.setNameIDPolicy(buildNameIdPolicy());
+
+        return authnRequest;
+    }
+
+    private static NameIDPolicy buildNameIdPolicy() {
+        NameIDPolicy nameIDPolicy = OpenSAMLUtils.buildSAMLObject(NameIDPolicy.class);
+        nameIDPolicy.setAllowCreate(true);
+        nameIDPolicy.setFormat(NameIDType.TRANSIENT);
+
+        return nameIDPolicy;
+    }
+
+    private static Issuer buildIssuer(String issuerId) {
+        Issuer issuer = OpenSAMLUtils.buildSAMLObject(Issuer.class);
+        issuer.setValue(issuerId);
+
+        return issuer;
+    }
+}
---
+++ java/cbsso-opensaml/app/src/main/java/cbsso/opensaml/OpenSAMLUtils.java
@@ -0,0 +1,87 @@
+package cbsso.opensaml;
+
+import javax.xml.namespace.QName;
+
+import org.opensaml.core.xml.XMLObject;
+import org.opensaml.core.xml.XMLObjectBuilderFactory;
+import org.opensaml.core.xml.config.XMLObjectProviderRegistrySupport;
+import org.opensaml.core.xml.io.Marshaller;
+import org.opensaml.core.xml.io.MarshallingException;
+import org.opensaml.saml.common.SignableSAMLObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Element;
+
+import net.shibboleth.utilities.java.support.security.impl.RandomIdentifierGenerationStrategy;
+import net.shibboleth.utilities.java.support.xml.SerializeSupport;
+
+public class OpenSAMLUtils {
+    private static Logger logger = LoggerFactory.getLogger(OpenSAMLUtils.class);
+    private static RandomIdentifierGenerationStrategy secureRandomIdGenerator;
+
+    static {
+        secureRandomIdGenerator = new RandomIdentifierGenerationStrategy();
+
+    }
+
+    public static <T> T buildSAMLObject(final Class<T> clazz) {
+        T object = null;
+        try {
+            XMLObjectBuilderFactory builderFactory = XMLObjectProviderRegistrySupport.getBuilderFactory();
+            QName defaultElementName = (QName) clazz.getDeclaredField("DEFAULT_ELEMENT_NAME").get(null);
+            object = (T) builderFactory.getBuilder(defaultElementName).buildObject(defaultElementName);
+        } catch (IllegalAccessException e) {
+            throw new IllegalArgumentException("Could not create SAML object");
+        } catch (NoSuchFieldException e) {
+            throw new IllegalArgumentException("Could not create SAML object");
+        }
+
+        return object;
+    }
+
+    public static String generateSecureRandomId() {
+        return secureRandomIdGenerator.generateIdentifier();
+    }
+
+    public static void logSAMLObject(final XMLObject object) {
+        Element element = null;
+
+        if (object instanceof SignableSAMLObject && ((SignableSAMLObject) object).isSigned()
+                && object.getDOM() != null) {
+            element = object.getDOM();
+        } else {
+            try {
+                Marshaller out = XMLObjectProviderRegistrySupport.getMarshallerFactory().getMarshaller(object);
+                out.marshall(object);
+                element = object.getDOM();
+
+            } catch (MarshallingException e) {
+                logger.error(e.getMessage(), e);
+            }
+        }
+        String xmlString = SerializeSupport.prettyPrintXML(element);
+
+        logger.info(xmlString);
+
+    }
+
+    public static String stringifySAMLObject(final XMLObject object) {
+        Element element = null;
+
+        if (object instanceof SignableSAMLObject && ((SignableSAMLObject) object).isSigned()
+                && object.getDOM() != null) {
+            element = object.getDOM();
+        } else {
+            try {
+                Marshaller out = XMLObjectProviderRegistrySupport.getMarshallerFactory().getMarshaller(object);
+                out.marshall(object);
+                element = object.getDOM();
+
+            } catch (MarshallingException e) {
+                logger.error(e.getMessage(), e);
+            }
+        }
+
+        return SerializeSupport.prettyPrintXML(element);
+    }
+}
